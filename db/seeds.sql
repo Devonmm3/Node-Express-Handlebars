eb16288b8eca0c89331c3a2bcf102b2d618d618e
DevonMM <Devon.m.moran@gmail.com>
2019-02-16 09:31:43 -0500

    js files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Mushroom', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Lentil', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Black Bean', false, CURRENT_TIMESTAMP);
